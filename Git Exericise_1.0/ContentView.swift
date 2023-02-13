06d7c84a3748c29c01d640f7705d97248566da10
peter <109552816+petermvgz@users.noreply.github.com>
2023-02-12 20:26:02 -0800

    Initial Commit

---
+++ Git Exericise_1.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Git Exericise_1.0
+//
+//  Created by Peter Deyi on 2/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Git Exericise_1.0/Git_Exericise_1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Git_Exericise_1_0App.swift
+//  Git Exericise_1.0
+//
+//  Created by Peter Deyi on 2/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Git_Exericise_1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
